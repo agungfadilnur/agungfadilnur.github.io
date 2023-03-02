c5a5ef00a000d2bfd4644e0f990a06e3e6b20b4c
Agung Nur Fachdillah <52580495+agungfadilnur@users.noreply.github.com>
2023-03-03 01:12:48 +0700

    Add files via upload

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230228.192037';
+const cacheName = 'chirpy-20230303.011009';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
